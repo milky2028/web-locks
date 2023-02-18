a895ccfee207908d0b0b9d15e500d99d7acb93dd
Tyler <tylergross28@gmail.com>
2023-02-17 19:00:20 -0500

    stuff?

--- server.ts
+++ server.ts
@@ -10,3 +10,5 @@ async function handler(): Promise<Response> {
 
 console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
 await serve(handler, { port });
+
+const someOtherStuff = 'stuff';
