0cfcf66dbec08c679de7454fd064d694c289e9a7
Tyler Gross <tylergross28@gmail.com>
2025-06-05 13:29:21 -0700

    old stuff

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ const port = 8080;
 async function handler(): Promise<Response> {
   const body = await Deno.readFile("./index.html");
 
-  return new Response(body, { status: 200 });
+  return new Response(body, { status: 200, headers: { 'Cross-Origin-Opener-Policy': 'same-origin', 'Cross-Origin-Embedder-Policy': 'require-corp' } });
 }
 
 console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
